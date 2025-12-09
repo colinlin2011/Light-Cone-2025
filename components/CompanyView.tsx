c3f36a43df2ce2e246c96f77f53b5eadf7accb70
colinlin2011 <45247184+colinlin2011@users.noreply.github.com>
2025-12-09 13:07:47 +0800

    Update CompanyView.tsx

--- components/CompanyView.tsx
+++ components/CompanyView.tsx
@@ -1,1 +1,10 @@
-// TODO: 添加代码
+// components/CompanyView.tsx
+// 暂时留空，后续开发
+export default function CompanyView() {
+  return (
+    <div>
+      <h2>公司视图</h2>
+      <p>正在开发中...</p>
+    </div>
+  );
+}
